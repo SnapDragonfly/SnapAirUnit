8ff97a03139621dfd208aec4522af8fccdc8cb2b
Daniel Li <lida_mail@163.com>
2022-12-26 09:10:17 +0800

    Add dirty flag to be 100% sure recompiled.

--- checkver.sh
+++ checkver.sh
@@ -2,6 +2,7 @@
 
 # for git repo dirty control
 rm -rf version.h
+rm -rf build/esp-idf/main/CMakeFiles/__idf_main.dir/factory_setting.c.obj
 
 if [[ $(git diff --stat) != '' ]]; then
 	echo -e "#define APP_DIRTYFLAG  \"dirty\"" > version.h
